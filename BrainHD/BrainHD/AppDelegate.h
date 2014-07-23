c94b8b25a17ac493f6b33fec72bb79504811e76b
QiuFeng <qfviolethill@163.com>
2014-07-23 12:32:52 +0800

    rename Brain HD to BrainHD

---
+++ BrainHD/BrainHD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BrainHD
+//
+//  Created by 邱峰 on 7/23/14.
+//  Copyright (c) 2014 TongjiUniversity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BrainHD/BrainHD/MainViewController.h
@@ -0,0 +1,14 @@
+//
+//  MainViewController.h
+//  Brain HD
+//
+//  Created by 邱峰 on 14-3-1.
+//  Copyright (c) 2014年 邱峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+
+@end
